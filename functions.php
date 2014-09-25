3b0cbac089c783fb06466d4c149e6c4757b3817e
Björn Martensen <bjoern.martensen@gmail.com>
2014-09-25 17:26:51 +0200

    add responsive image support and a basic job template

--- functions.php
+++ functions.php
@@ -136,6 +136,7 @@ class StarterSite extends TimberSite
         /* this is where you can add your own fuctions to twig */
         $twig->addExtension(new Twig_Extension_StringLoader());
         $twig->addFilter('myfoo', new Twig_Filter_Function('myfoo'));
+        $twig->addFunction(new Twig_SimpleFunction('srcset_post_thumbnail', 'srcset_post_thumbnail'));
         return $twig;
     }
 
@@ -162,4 +163,44 @@ function myfoo($text)
 {
     $text .= ' bar!';
     return $text;
+}
+
+function srcset_post_thumbnail($defaultSize = 'medium')
+{
+    $thumbnailSizes = [
+        'thumbnail',
+        'medium',
+        'large',
+        'full'
+    ];
+
+    $html = '<img sizes="';
+    $html .= '(max-width: 30em) 100vw, ';
+    $html .= '(max-width: 50em) 50vw, ';
+    $html .= 'calc(33vw - 100px)" ';
+    $html .= 'srcset="';
+
+    $thumb_id = get_post_thumbnail_id();
+
+    for ($i = 0; $i < count($thumbnailSizes); $i++) {
+        $thumb = wp_get_attachment_image_src($thumb_id, $thumbnailSizes[$i], true);
+
+        $url = $thumb[0];
+        $width = $thumb[1];
+
+        $html .= $url . ' ' . $width . 'w';
+
+        if ($i < count($thumbnailSizes) - 1) {
+            $html .= ', ';
+        }
+    }
+
+    $alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
+
+    $thumbMedium = wp_get_attachment_image_src($thumb_id, $defaultSize, true);
+
+    $html .= '" ';
+    $html .= 'src="' . $thumbMedium[0] . '" alt="' . $alt . '">';
+
+    return $html;
 }
