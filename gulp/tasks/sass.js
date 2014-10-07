cd860edeaa55bbba13709c14d0ef08e46e6871bc
Björn Martensen <bjoern.martensen@gmail.com>
2014-10-07 10:54:40 +0200

    improve autoprefixer settings

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -19,7 +19,7 @@ gulp.task('sass', function () {
     return gulp.src(inputDir + '/sass/*.scss')
         .pipe(sass(config))
         .on('error', handleErrors)
-        .pipe(autoprefix('last 10 version'))
+        .pipe(autoprefix('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
         .pipe(gulp.dest(outputDir + '/css'))
         .pipe(notify('CSS compiled, prefixed and minified.'));
 
